8365f3e98d971ba9659af94729541d2a97dd70a1
MacBook Pro <macbookpro@192.168.1.8>
2024-04-04 16:43:14 -0500

    First commit

---
+++ tests/basic_test.spec.js
@@ -0,0 +1,11 @@
+const {test} = require('@playwright/test');
+
+
+test('First playwright test', async ({ page }) => {
+    //const context = await browser.newContext();
+    //const page = await context.newPage();
+    //In case we don't have any requirement like cookies or something to put into the browser
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+
+})
