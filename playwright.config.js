db3bec0b6424dbaabb4e531b38fb39618ba8f287
MacBook Pro <macbookpro@192.168.1.8>
2024-04-05 16:03:26 -0500

    added some configuration

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+  /* Run tests in files in parallel */  
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -38,15 +38,15 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    //{
+    //  name: 'firefox',
+    //  use: { ...devices['Desktop Firefox'] },
+    //},
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    //{
+    //  name: 'webkit',
+    //  use: { ...devices['Desktop Safari'] },
+    //},
 
     /* Test against mobile viewports. */
     // {
--- tests/basic_test.spec.js
+++ tests/basic_test.spec.js
@@ -1,4 +1,4 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 
 test('First playwright test', async ({ page }) => {
@@ -6,6 +6,8 @@ test('First playwright test', async ({ page }) => {
     //const page = await context.newPage();
     //In case we don't have any requirement like cookies or something to put into the browser
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
 
 
 })
